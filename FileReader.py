5bbd5484b7e7f9766ced62d3cd81c7784e5adf0c
Hardik Pawar <hardikhpawar.cs21@rvce.edu.in>
2023-09-22 17:13:27 +0530

    Add individual face hover and download feature

--- FileReader.py
+++ FileReader.py
@@ -49,14 +49,19 @@ if __name__ == "__main__":
 
     app_py = "app.py"
     count_faces_py = "CountFaces.py"
+    
     index_html = "templates/index.html"
+    
     index_css = "static/css/index.css"
+    face_css = "static/css/face.css"
+    
     index_js = "static/js/index.js"
 
     file_list = [
         app_py,
         count_faces_py,
         index_html,
+        face_css
     ]
 
     read_files_in_directory(".", file_list)
