37757e6ba3a2892c5f9a953cb5437cfb181e0680
Hardik Pawar <hardikhpawar.cs21@rvce.edu.in>
2023-09-24 08:52:17 +0530

    Add sticky arrow

--- FileReader.py
+++ FileReader.py
@@ -58,8 +58,8 @@ if __name__ == "__main__":
     index_js = "static/js/index.js"
 
     file_list = [
-        app_py,
-        count_faces_py
+        index_html,
+        app_py
     ]
 
     read_files_in_directory(".", file_list)
